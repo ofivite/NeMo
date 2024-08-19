80023b6cc8350c060938945b976fad1d6067acae
ofivite <20295221+ofivite@users.noreply.github.com>
2024-08-19 21:38:01 +0200

    mlflow updates

--- nemo/utils/loggers/mlflow_logger.py
+++ nemo/utils/loggers/mlflow_logger.py
@@ -20,7 +20,7 @@ from typing import Any, Dict, Optional
 class MLFlowParams:
     # name of experiment, if none, defaults to the globally set experiment name
     experiment_name: Optional[str] = None
-    # no run_name because it's set by version
+    run_name: Optional[str] = None
     # local or remote tracking seerver. If tracking_uri is not set, it defaults to save_dir
     tracking_uri: Optional[str] = None
     tags: Optional[Dict[str, Any]] = None
